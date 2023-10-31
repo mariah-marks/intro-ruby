5ddcc2964f02553951e10bb0c252a7f33331752e
Mariah <139914168+mariah-marks@users.noreply.github.com>
2023-10-31 09:38:33 -0400

    Add variable exercises and update 1-basics

--- 1-basics/5.rb
+++
@@ -1,9 +0,0 @@
-fact_5 = 1 * 2 * 3 * 4 * 5
-fact_6 = 1 * 2 * 3 * 4 * 5 * 6
-fact_7 = 1 * 2 * 3 * 4 * 5 * 6 * 7
-fact_8 = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8
-
-puts fact_5
-puts fact_6
-puts fact_7
-puts fact_8
---
+++ 2-variables/name.rb
@@ -0,0 +1,10 @@
+puts 'What\'s your first name?'
+first_name = gets.chomp
+puts 'What\'s your last name?'
+last_name = gets.chomp
+
+puts 'Hi ' + first_name + ' ' + last_name
+
+10.times do
+  puts first_name + last_name
+end
