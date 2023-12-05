2a304e4d8ee364385c5140c9beaded5534f7e5e2
Mariah <139914168+mariah-marks@users.noreply.github.com>
2023-12-05 07:57:43 -0500

    Check strings for regex match.

---
+++ 8-more-stuff/1.rb
@@ -0,0 +1,12 @@
+def has_lab?(word)
+  if word =~ /lab/
+    puts word
+  end
+end
+
+has_lab?("laboratory")
+has_lab?("experiment")
+has_lab?("Pans Labyrinth")
+has_lab?("elaborate")
+has_lab?("polar bear")
+
